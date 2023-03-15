f1f316a5b421a69132ffdd2a575a280ec71fbccf
Rohit Tokala <rtokala@purdue.edu>
2023-03-15 18:37:06 -0400

    Setup Redux

---
+++ src/components/SSRPokemonTable.tsx
@@ -0,0 +1,11 @@
+import PokemonTable from "./PokemonTable";
+
+function SSRPokemonTable() {
+  return (
+    <div>
+      <PokemonTable pokemons={[]} />
+    </div>
+  );
+}
+
+export default SSRPokemonTable;
