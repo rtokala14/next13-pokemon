056adfe111e6167b375689347f45c5ed55b11775
Rohit Tokala <rtokala@purdue.edu>
2023-03-15 23:30:01 -0400

    Integrate RTK

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,8 +4,11 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 import { AppDispatch, RootState } from "@/store";
 import { setSearch } from "@/store/searchSlice";
+import { pokemonApi } from "@/store/pokemonApi";
 
 import PokemonTable from "./PokemonTable";
+import { type Pokemon } from "@/types";
+import { useEffect } from "react";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
@@ -14,6 +17,14 @@ function SearchInput() {
   const dispatch = useAppDispatch();
   const search = useAppSelector((state) => state.search.search);
   const startupPokemon = useAppSelector((state) => state.search.startupPokemon);
+  const data = useAppSelector(
+    (state) =>
+      state.pokemonApi.queries[`search("${search})"`]?.data as Pokemon[]
+  );
+
+  useEffect(() => {
+    dispatch(pokemonApi.endpoints.search.initiate(search));
+  }, [dispatch, search]);
 
   return (
     <div>
@@ -22,7 +33,7 @@ function SearchInput() {
         value={search}
         onChange={(e) => dispatch(setSearch(e.target.value))}
       />
-      <PokemonTable pokemons={startupPokemon} />
+      <PokemonTable pokemons={search.length ? data ?? [] : startupPokemon} />
     </div>
   );
 }
